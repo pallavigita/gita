60c59958f99dbdd2a5f5314b230980113199a93c
unknown <you@example.com>
2018-08-29 23:33:24 -0700

    project commit

---
+++ my-project/src/app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <skills/>
+</template>
+
+<script>
+import skills from'./components/skills.vue '
+
+export default{ 
+  name: 'App',
+  components: {
+    skills
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
